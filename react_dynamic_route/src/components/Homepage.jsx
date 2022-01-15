34657f8dd767f4322e59800338c481dcc792d3ad
Prasad Baviskar <71400522+PrasadBaviskar@users.noreply.github.com>
2022-01-15 14:19:51 +0530

    add  dynamic routing asg
    Create the following routes
    
    Home - /
    
    All Products - /products
    
    Product details - /products/:id
    
    The All Products page should display the data (end of the question) in a tabular manner.
    
    Product Name | Price | more details
    
    on clicking the more details take user to /products/:id route
    
    use useParams hook

---
+++ react_dynamic_route/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+export const Homepage = () => {
+  return (
+    <>
+      <h1>Homepage</h1>
+      <img
+        src="https://cdn6.f-cdn.com/contestentries/1030409/25006549/5925da92acd8f_thumb900.jpg"
+        alt="homepage"
+      />
+    </>
+  );
+};
